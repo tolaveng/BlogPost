f478bfc6458eea7fd4310e5ad820ecaf8b03fed6
tolaveng <tolaveng@hotmail.com>
2023-12-11 21:01:11 +1100

    Add modal

---
+++ WebApp/Components/TModal.razor.js
@@ -0,0 +1,10 @@
+﻿export const TModal = {
+    show: function (modalRef) {
+        const modal = bootstrap.Modal.getOrCreateInstance(modalRef);
+        modal.show();
+    },
+    close: (modalRef) => {
+        const modal = bootstrap.Modal.getOrCreateInstance(modalRef);
+        modal.hide();
+    }
+}

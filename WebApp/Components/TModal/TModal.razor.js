f75c5611a6d7ed59c20619ab030cca2b9f73393e
tolaveng <tolaveng@hotmail.com>
2023-12-23 08:26:54 +1100

    Update TModal

---
+++ WebApp/Components/TModal/TModal.razor.js
@@ -0,0 +1,19 @@
+﻿export function TModal_init(dotNetRef) {
+    const dotNet = dotNetRef;
+
+    return {
+        show: function (modalRef) {
+            const modal = bootstrap.Modal.getOrCreateInstance(modalRef);
+            modal.show();
+            // handle modal close
+            modalRef.addEventListener('hidden.bs.modal', evt => {
+                dotNet.invokeMethodAsync('OnModalCloseJs');
+            });
+        },
+        close: (modalRef) => {
+            const modal = bootstrap.Modal.getOrCreateInstance(modalRef);
+            modal.hide();
+            dotNet.invokeMethodAsync('OnModalCloseJs');
+        }
+    }
+}

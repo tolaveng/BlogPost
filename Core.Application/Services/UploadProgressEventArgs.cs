312f984b61aa4dbe53d010c28b3b916e19ac0aa5
tolaveng <tolaveng@hotmail.com>
2024-03-31 10:58:31 +1100

    Replace Azure storage by Cloudinary

---
+++ Core.Application/Services/UploadProgressEventArgs.cs
@@ -0,0 +1,14 @@
+﻿namespace Core.Application.Services
+{
+    public class UploadProgressEventArgs
+    {
+        public string FileName { get; set; }
+        public int Progress { get; set; }
+
+        public UploadProgressEventArgs(string fileName, int progress)
+        {
+            FileName = fileName;
+            Progress = progress;
+        }
+    }
+}
